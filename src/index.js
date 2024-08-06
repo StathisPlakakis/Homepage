ad958e598eade71eaeaa6c43112fc3d254f42212
StathisPlakakis <stathisplakorg1997@gmail.com>
2024-08-06 21:12:54 +0300

    Add the header image

--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
 import './style.css';
 import headerImage from './assets/images&icons/header.jpg';
+import headerImageZoom from './assets/images&icons/headerZoom.jpg';
 
-const headerInfo = document.querySelector('.header-info');
+const headerImg = document.querySelector('.header-img');
+const picture = document.createElement('picture');
+const source = document.createElement('source');
+source.srcset = headerImage;
+source.media = '(min-width: 500px)';
+picture.appendChild(source);
+const img = document.createElement('img');
+img.src = headerImageZoom;
+picture.appendChild(img);
+headerImg.prepend(picture);
