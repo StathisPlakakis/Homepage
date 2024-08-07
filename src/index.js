5a327be0bc61835d8abcc0bc5505c3f7eaa671a3
StathisPlakakis <stathisplakorg1997@gmail.com>
2024-08-07 17:51:58 +0300

    Implement the design for header on mobile screens

--- src/index.js
+++ src/index.js
@@ -1,14 +1,8 @@
 import './style.css';
-import headerImage from './assets/images&icons/header.jpg';
 import headerImageZoom from './assets/images&icons/headerZoom.jpg';
 
 const headerImg = document.querySelector('.header-img');
-const picture = document.createElement('picture');
-const source = document.createElement('source');
-source.srcset = headerImage;
-source.media = '(min-width: 500px)';
-picture.appendChild(source);
-const img = document.createElement('img');
-img.src = headerImageZoom;
-picture.appendChild(img);
-headerImg.prepend(picture);
+headerImg.style.backgroundImage = `url(${headerImageZoom})`;
+headerImg.style.backgroundPosition = 'center';
+headerImg.style.backgroundSize = 'contain';
+headerImg.style.backgroundRepeat = 'no-repeat';
