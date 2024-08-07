44586a31ae3f164935e9ce6fd55f000258efd8ca
StathisPlakakis <stathisplakorg1997@gmail.com>
2024-08-07 19:13:12 +0300

    Config webpack to handle imgs direct from css

--- src/index.js
+++ src/index.js
@@ -1,8 +1,1 @@
 import './style.css';
-import headerImageZoom from './assets/images&icons/headerZoom.jpg';
-
-const headerImg = document.querySelector('.header-img');
-headerImg.style.backgroundImage = `url(${headerImageZoom})`;
-headerImg.style.backgroundPosition = 'center';
-headerImg.style.backgroundSize = 'contain';
-headerImg.style.backgroundRepeat = 'no-repeat';
--- webpack.common.js
+++ webpack.common.js
@@ -1,34 +1,43 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
- module.exports = {
-    entry: './src/index.js',
-    output: {
-        filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-        clean: true,
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index.html',
+      inject: 'body',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
       },
-    plugins: [
-        new HtmlWebpackPlugin ({
-            template: './src/index.html',
-            filename: 'index.html',
-            inject: 'body'
-        }) 
-    ],
-    module: {
-        rules: [
-        {
-            test: /\.css$/i,
-            use: ['style-loader', 'css-loader'],
-        },
-        {
-            test: /\.(png|svg|jpg|jpeg|gif)$/i,
-            type: 'asset/resource',
-        },
-        {
-            test: /\.(woff|woff2|eot|ttf|otf)$/i,
-            type: 'asset/resource',
-        },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[hash].[ext]',
+              outputPath: './assets/images&icons',
+            },
+          },
         ],
-    },
- };
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+};
