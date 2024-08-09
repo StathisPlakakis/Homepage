589ce61a6eefc4501742c72a3c2d5fdd739fd859
StathisPlakakis <stathisplakorg1997@gmail.com>
2024-08-09 20:06:58 +0300

    Comment the snippet that handles files with file-loader

--- webpack.common.js
+++ webpack.common.js
@@ -24,15 +24,15 @@ module.exports = {
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: '[name].[hash].[ext]',
-              outputPath: './assets/images&icons',
-            },
-          },
-        ],
+        // use: [
+        //   {
+        //     loader: 'file-loader',
+        //     options: {
+        //       name: '[name].[hash].[ext]',
+        //       outputPath: './assets/images&icons',
+        //     },
+        //   },
+        // ],
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
